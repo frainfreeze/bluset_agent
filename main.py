3b06ae3128e9eddf38ec3e96198e784c4cbfcd47
frainfreeze <kucartomislav+github@gmail.com>
2021-02-02 23:22:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import os
 import sys
+import json
 
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 
 cwd = os.getcwd()
 app = Flask(__name__, static_url_path=cwd)
+LOG_FILE = "/var/log/nginx/access.log"
+FILE_NAME = "stats_report"
 
 # fix for goacess debian package bug https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=934121
-@app.route('/fonts/<path:path>')
+@app.route("/fonts/<path:path>")
 def send_js(path):
-    return send_from_directory(os.path.join(cwd,'fonts'), path)
+    return send_from_directory(os.path.join(cwd,"fonts"), path)
 
-@app.route('/')
-def root():
+@app.route("/")
+def index():
     try:
-        t = os.system('goaccess /var/log/nginx/access.log -o stats_report.html --log-format=COMBINED')
-        return send_from_directory(cwd, "stats_report.html")
+        t = os.system(f"goaccess {LOG_FILE} -o {FILE_NAME}.html --log-format=COMBINED")
+        return send_from_directory(cwd, f"{FILE_NAME}.html")
     except:
         return "No stats."
 
-if __name__ == '__main__':
+@app.route("/json")
+def json_data():
+    try:
+        t = os.system("goaccess {LOG_FILE} -o {FILE_NAME}.json --log-format=COMBINED")
+        with open(f"{FILE_NAME}.json") as f:
+            data = json.load(f)
+        return jsonify(data)
+    except:
+        return "No stats."
+
+        
+if __name__ == "__main__":
     if len(sys.argv) > 1:
         port = int(sys.argv[1])
     else:
